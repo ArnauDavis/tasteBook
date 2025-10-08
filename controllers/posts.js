5d72022cf5f57328f752f094e1100b0b711cc8b3
Arnau Davis <arnau.davis@outlook.com>
2025-10-08 05:51:09 -0400

    added a sidebar nav with correctly active classes for tabs, and moved routes to render inside of mainDash

--- controllers/posts.js
+++ controllers/posts.js
@@ -5,7 +5,7 @@ module.exports = {
   getProfile: async (req, res) => {
     try {
       const posts = await Post.find({ user: req.user.id });
-      res.render("profile.ejs", { posts: posts, user: req.user });
+      res.render("mainDash.ejs", {page: req.url, posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
     }
@@ -13,7 +13,7 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      res.render("feed.ejs", { posts: posts });
+      res.render("mainDash.ejs", {page: req.url, posts: posts });
     } catch (err) {
       console.log(err);
     }
