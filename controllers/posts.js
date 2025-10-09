a5f60ceb4b097b4280543f68baf7bfb29de5f38a
Arnau Davis <arnau.davis@outlook.com>
2025-10-09 18:05:17 -0400

    fixed bugs spawned from trying new branches

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,6 +1,6 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
-
+const User = require("../models/User")
 module.exports = {
   getProfile: async (req, res) => {
     try {
@@ -13,7 +13,6 @@ module.exports = {
   getFeed: async (req, res) => {
     try {
       const posts = await Post.find().sort({ createdAt: "desc" }).lean();
-      console.log(req.url)
       res.render("mainDash.ejs", {page: req.url, posts: posts });
     } catch (err) {
       console.log(err);
@@ -22,8 +21,12 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
+      const postCreator = await User.findById(post.user)
+      const postProfile = postCreator.id 
+      const userName = postCreator.userName
+      const visitorId = req.user.id
       // const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
-      res.render("post.ejs", {page: req.url, post: post, user: req.user});
+      res.render("post.ejs", {page: req.url, post: post, user: userName, postProfile: postProfile, visitorId:visitorId});
     } catch (err) {
       console.log(err);
     }
