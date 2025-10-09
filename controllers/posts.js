09286a14d8f4dac92433f7e8fe93c149c555f159
Arnau Davis <arnau.davis@outlook.com>
2025-10-09 10:06:01 -0400

    Revert "added post creator to title and sidenav bar while also preventing favoriting bug, almost had conditional checking for poster id as opposed to the requesting user id"
    This reverts commit 91647c3c88f76c04755182ec15ad99febbad2c8a.

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,12 +1,10 @@
 const cloudinary = require("../middleware/cloudinary");
 const Post = require("../models/Post");
-const User = require("../models/User")
 
 module.exports = {
   getProfile: async (req, res) => {
     try {
       const posts = await Post.find({ user: req.user.id });
-      
       res.render("mainDash.ejs", {page: req.url, posts: posts, user: req.user });
     } catch (err) {
       console.log(err);
@@ -24,13 +22,8 @@ module.exports = {
   getPost: async (req, res) => {
     try {
       const post = await Post.findById(req.params.id);
-      const postCreator = await User.findById(post.user)
-      const postProfile = postCreator.id 
-      const userName = postCreator.userName
-      const visitorId = req.user.id
-      console.log(post)
       // const comments = await Comment.find({post: req.params.id}).sort({ createdAt: "desc" }).lean();
-      res.render("mainDash.ejs", {page: post.id, post: post, user: userName, postProfile: postProfile, visitorId:visitorId});
+      res.render("post.ejs", {page: req.url, post: post, user: req.user});
     } catch (err) {
       console.log(err);
     }
