6d5f75b67b814a5693b90f2a58335df35a4e7ae4
Arnau Davis <arnau.davis@outlook.com>
2025-10-24 03:28:27 -0400

    added basic frame for comments, not yet added to views, concerned about server.js comments route

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const connectDB = require("./config/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
 const editProfileRoutes = require("./routes/editProfile");
+const commentRoutes = require("./routes/comments");
 
 
 //Use .env file in config folder
@@ -60,6 +61,7 @@ app.use(flash());
 app.use("/", mainRoutes);
 app.use("/post", postRoutes);
 app.use('/editProfile', editProfileRoutes);
+app.use("/comment", commentRoutes);
 
 
 //Server Running
