99ab44fa9b5a49db80ab05f0c5d0ae2b76e94a44
Arnau Davis <arnau.davis@outlook.com>
2026-03-14 19:17:00 -0400

    Fixed routing reload problem, if app spins down, users will be directed to login page instead of the app refreshing

--- server.js
+++ server.js
@@ -1,44 +1,44 @@
-const express = require("express");
-const app = express();
-const mongoose = require("mongoose");
-const passport = require("passport");
-const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
-const methodOverride = require("method-override");
-const flash = require("express-flash");
-const logger = require("morgan");
-const connectDB = require("./config/database");
-const mainRoutes = require("./routes/main");
-const postRoutes = require("./routes/posts");
-const editProfileRoutes = require("./routes/editProfile");
-const commentRoutes = require("./routes/comments");
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const passport = require("passport")
+const session = require("express-session")
+const MongoStore = require("connect-mongo")(session)
+const methodOverride = require("method-override")
+const flash = require("express-flash")
+const logger = require("morgan")
+const connectDB = require("./config/database")
+const mainRoutes = require("./routes/main")
+const postRoutes = require("./routes/posts")
+const editProfileRoutes = require("./routes/editProfile")
+const commentRoutes = require("./routes/comments")
 
 
 //Use .env file in config folder
-require("dotenv").config({ path: "./config/.env" });
-const port = process.env.PORT || 8000;
+require("dotenv").config({ path: "./config/.env" })
+const port = process.env.PORT || 8000
 
 // Passport config
-require("./config/passport")(passport);
+require("./config/passport")(passport)
 
 //Connect To Database
-connectDB();
+connectDB()
 
 //Using EJS for views
-app.set("view engine", "ejs");
+app.set("view engine", "ejs")
 
 //Static Folder
-app.use(express.static("public"));
+app.use(express.static("public"))
 
 //Body Parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 
 //Logging
-app.use(logger("dev"));
+app.use(logger("dev"))
 
 //Use forms for put / delete
-app.use(methodOverride("_method"));
+app.use(methodOverride("_method"))
 
 // Setup Sessions - stored in MongoDB
 app.use(
@@ -51,20 +51,26 @@ app.use(
 );
 
 // Passport middleware
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(passport.initialize())
+app.use(passport.session())
 
 //Use flash messages for errors, info, ect...
-app.use(flash());
+app.use(flash())
 
 //Setup Routes For Which The Server Is Listening
-app.use("/", mainRoutes);
-app.use("/post", postRoutes);
-app.use('/editProfile', editProfileRoutes);
-app.use("/comment", commentRoutes);
+app.use("/", mainRoutes)
+app.use("/post", postRoutes)
+app.use('/editProfile', editProfileRoutes)
+app.use("/comment", commentRoutes)
 
+
+//
+
+app.get('*', (req, res) => {
+  res.redirect('/')
+})
 
 //Server Running
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+  console.log(`Server is running on port ${port}`)
+})
